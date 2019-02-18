163508640af5474f68537fadeca338d305b8a18f
rayGreycloud <rayGreycloud@gmail.com>
2019-02-17 20:05:09 -0800

    Add server

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLICATION_ID || '45753ed6428d5bec10d454c5581c2133d114976a03b7826c2660950435a3f750',
+  APP_SECRET: process.env.APP_SECRET || 'd918d28c7c080df5e74de158ab71744164e341ee670f08aa2caf503052e184b0',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
